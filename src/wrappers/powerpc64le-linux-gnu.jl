38184386dec43fc5c9ac15dbba63b6cd581e7ad3
jlbuild <juliabuildbot@gmail.com>
2021-09-29 22:20:58 +0000

    Libbpf_jll build 0.5.0+0

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Elfutils_jll)
     JLLWrappers.@init_library_product(
         libbpf,
-        "lib/libbpf.so",
+        "lib64/libbpf.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
